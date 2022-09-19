cb8ccd443fc084faaa12c5633c0663945b2c233d
User <User@DESKTOP-H615SEH>
2022-09-19 12:43:03 +0300

    Добавьте файлы проекта.

---
+++ PONID/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace Polindroms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a palindrome");
+            string polindrom = Console.ReadLine();
+            char[] mid = polindrom.ToCharArray();
+            Array.Reverse(mid);
+            string total = new string(mid);
+            if (polindrom == total)
+            {
+                Console.WriteLine("TRUE");
+            }
+            else
+            {
+                Console.WriteLine("FALSE");
+            }
+            Console.ReadKey();
+        }
+    }
+}
